85930a1c6186e6e53807dec7c0f538ae5afdede8
lmuench <muench.ludwig@gmail.com>
2019-08-02 15:02:32 -0400

    use Request and Response structs from http package

---
+++ test/session_test.go
@@ -0,0 +1,41 @@
+package test
+
+import (
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/lmuench/api/api"
+)
+
+var cookie1 = &http.Cookie{
+	Name:  "api_test_session_cookie",
+	Value: "abc123",
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, cookie1)
+}
+
+func runServer() {
+	http.HandleFunc("/foo", handler)
+	log.Fatal(http.ListenAndServe(":6000", nil))
+}
+
+func TestSessionTokens(t *testing.T) {
+	go runServer()
+
+	call := api.Call{
+		Command: "GET",
+		Args:    []string{"http://localhost:6000/foo"},
+	}
+	api.Handle(call)
+	answer2 := api.Handle(call)
+	cookie2, err := answer2.Response.Request.Cookie(cookie1.Name)
+	if err != nil {
+		t.Error(err)
+	}
+	if cookie2.Value != cookie1.Value {
+		t.Error("Second request did not contain cookie returned by first response")
+	}
+}
